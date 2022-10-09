4fb7c84e47836020a63db55b7f2a75ce9ef71b60
Susheel Kumar <developer.susheel@gmail.com>
2022-10-10 00:37:20 +0530

    Routes Created

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+
+const Home = () => (
+  <div>
+    <h1 className="text-5xl">Test</h1>
+  </div>
+);
+
+export default Home;
---
+++ pages/my-nfts.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MyNFTs = () => (
+  <div>
+    My NFTs
+  </div>
+);
+
+export default MyNFTs;
