5e62dee0284a59891eef05e30182c4ab0d722a87
Susheel Kumar <developer.susheel@gmail.com>
2022-10-10 01:04:41 +0530

    Navbar Routes Added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import Script from 'next/script';
+import { ThemeProvider } from 'next-themes';
+import '../styles/globals.css';
+import { Navbar, Footer } from '../components';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+    <Script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" crossOrigin="anonymous" />
+  </ThemeProvider>
+);
 
-export default MyApp
+export default MyApp;
